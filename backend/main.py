96c13f68789decd73e5d57311551841bb4017bd8
hirakae1207 <hirakae1207@gmail.com>
2026-08-08 22:09:08 +0900

    Add Gemini API test endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return{"message": "Hello, ai-guess-game!"}
+
+@app.get("/test-gemini")
+def test_gemini():
+    response = client.models.generate_content(
+    model= "gemini-3.1-flash-lite",
+    contents= "あなたはマジカルバナナに似たゲームに参加しています。あなたのほかに３人の人間がいます。あなたは３人からキーワードをもらい、キーワードからより連想しやすいお題を応えます。テーマにあったお題が２つ出されました。テーマはフルーツ。お題は「りんご」と「梨」です。３人からのキーワードは「赤い」「iphone」「緑」でした。りんごと梨のどちらに近い？")
+    return {"response": response.text}
