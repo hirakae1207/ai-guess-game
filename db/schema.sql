912917b419314cd9fc14216e1e8fa36c1de1e94e
hirakae1207 <hirakae1207@gmail.com>
2026-08-09 18:03:50 +0900

    Add MySQL schema and connection setup

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Themes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    theme VARCHAR(64)
+);
+
+CREATE TABLE Topics(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    theme_id INT,
+    topic_text VARCHAR(64),
+    FOREIGN KEY (theme_id) REFERENCES Themes(id)
+);
+
+CREATE TABLE Topic_pairs(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    theme_id INT,
+    topic1_id INT,
+    topic2_id INT,
+    FOREIGN KEY (theme_id) REFERENCES Themes(id),
+    FOREIGN KEY (topic1_id) REFERENCES Topics(id),
+    FOREIGN KEY (topic2_id) REFERENCES Topics(id)
+);
+
+CREATE TABLE Players(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(64),
+    pair_id INT,
+    assigned_topic_id INT,
+    keyword VARCHAR(64),
+    FOREIGN KEY (pair_id) REFERENCES Topic_pairs(id),
+    FOREIGN KEY (assigned_topic_id) REFERENCES Topics(id)
+);
