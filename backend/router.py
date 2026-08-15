ed9e4c3c62f1c3a55f220b8635d948c21806af26
hirakae1207 <hirakae1207@gmail.com>
2026-08-16 00:37:35 +0900

    処理の1~4を変更確認済み

--- backend/router.py
+++ backend/router.py
@@ -7,7 +7,7 @@ import crud
 router = APIRouter()
 
 # DBをpythonで扱えるようにする
-DB_URL = "mysql+pymysql://root@db:3306/db?charset=utf8"
+DB_URL = f"mysql+pymysql://root:db1207@db:3306/ai_guess_game?charset=utf8"
 
 db_engine = create_engine(DB_URL, echo=True)
 db_session = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
@@ -25,3 +25,27 @@ def get_all_theme(
     db: Session = Depends(get_db),
 ):
     return crud.get_all_themes(db)
+
+#playerのnameの追加
+@router.post("/player")
+def create_name(
+    content:dict = Body(),
+    db:Session = Depends(get_db)
+):
+    print("受けたデータ\n", content)
+    result = crud.create_name(db, content)
+
+    print("返すデータ\n", result)
+    return result
+
+# playerのnameの表示
+@router.get("/player/{player_id}")
+def get_name(
+    player_id: int,
+    db: Session = Depends(get_db)
+):
+    result = crud.get_name(db, player_id=player_id)
+
+    if result is None:
+        raise HTTPException(status_code=404, detail="player not found")
+    return result
