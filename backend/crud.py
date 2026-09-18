a0e546ee2533e590116d4bf2b00816caa8f876d6
hirakae1207 <hirakae1207@gmail.com>
2026-09-18 10:53:26 +0900

    make add assigned and random system

--- backend/crud.py
+++ backend/crud.py
@@ -17,8 +17,20 @@ def get_all_themes(
     return result
 
 #ラウンド追加
-def create_game(db):
-    result = db.execute(text("INSERT INTO Games () VALUES ()"))
+def create_game(
+        db:Session,
+        content: dict
+):
+    sql = text(
+        """
+        INSERT INTO Games(theme_id)VALUES(:theme_id)
+        """
+    )
+    params= {
+        "theme_id":content.get("theme_id")
+    }
+    print(f"SQL:{sql}\nparams: {params}")
+    result = db.execute(sql, params)
     db.commit()
     new_game_id = result.lastrowid
     return new_game_id
@@ -53,12 +65,13 @@ def create_name(
 ):
     sql = text(
         """
-        INSERT INTO Players(name)
-        VALUES(:name)
+        INSERT INTO Players(name, game_id)
+        VALUES(:name, :game_id)
         """
     )
     params = {
-        "name": content.get("name")
+        "name": content.get("name"),
+        "game_id": content.get("game_id")
     }
 
     print(f"SQL: {sql}\nPrams: {params}")
@@ -71,6 +84,8 @@ def create_name(
 
     new_player = get_name(db, player_id=new_player_id)
     print(f"DB操作の結果: {new_player}")
+
+    return new_player
 
 
 #playerのnameを１人分だけ表示
@@ -96,6 +111,25 @@ def get_name(
 
     return result
 
+# Player.id, name, assigned_topic_idを表示
+def get_player_topic_by_game(
+        db:Session,
+        game_id: int
+):
+    sql = text(
+        """
+        SELECT id, name, assigned_topic_id from Players
+        WHERE game_id = :game_id
+        """
+    )
+    params = {"game_id": game_id}
+
+    print(f"SQL: {sql}\n Params: {params}")
+    result = db.execute(sql, params).mappings().all()
+
+    print(f"DB操作の結果: {result}")
+
+    return result
 
 # playerをお題へ割り振り
 import random
@@ -114,6 +148,92 @@ def assign_groups(player_name, pattern):
 
     return assignment
 
-def assign_groups_and_save(db):
-    # DBからプレイヤー名の一覧を取得する
-    players = db.execute(text("SELECT id, name FROM Players ORDER BY id DESC LIMIT 3")).mappings().all()
+def assign_groups_and_save(
+        db, game_id):
+    # game_idに紐づくプレイヤーの取得
+    sql = text(
+        """
+        SELECT id, name from Players
+        WHERE game_id = :game_id
+        """
+    )
+    params = {"game_id": game_id}
+    print(f"SQL: {sql}\nParams: {params}")
+    players =db.execute(sql, params).mappings().all()
+    player_names = [p["name"] for p in players]
+    player_id = {p["name"]: p["id"] for p in players}
+
+    # game_idに紐づくtheme_idの取得
+    sql_theme = text(
+        """
+        SELECT theme_id from Games WHERE id = :game_id
+        """
+    )
+    params_theme = {"game_id": game_id}
+    print(f"SQL: {sql_theme}\nParams: {params_theme}")
+    theme_id_row = db.execute(sql_theme, params_theme).mappings().first()
+    theme_id = theme_id_row["theme_id"]
+
+    # テーマのトピックのペアを取得
+    sql_pair = text(
+        """
+        SELECT topic1_id, topic2_id from Topic_pairs
+        WHERE theme_id = :theme_id
+        """
+    )
+    params_pair = {"theme_id": theme_id}
+    print(f"SQL: {sql_pair}\nParams: {params_pair}")
+    pair = db.execute(sql_pair, params_pair).mappings().first()
+    topic1_id = pair["topic1_id"]
+    topic2_id = pair["topic2_id"]
+
+
+    # 割り振りパターンを決めて上記の関数を呼び出す
+    pattern = ["A", "A", "B"]
+    assignment = assign_groups(player_names, pattern)
+
+    # "A"/"B"を実際のTopics.idに変換
+    topic_id = {
+        "A": topic1_id,
+        "B": topic2_id
+    }
+
+    # 結果をDBに保存
+    for name, label in assignment.items():
+        sql_db = text(
+            """
+            UPDATE Players SET assigned_topic_id = :assigned_topic_id WHERE id = :player_id
+            """
+        )
+        params_db = {"assigned_topic_id": topic_id[label], "player_id": player_id[name]}
+        print(f"SQL; {sql_db}\nParams: {params_db}")
+        db.execute(sql_db, params_db)
+    db.commit()
+    result = get_player_topic_by_game(db, game_id=game_id)
+
+    return result
+
+# playerのtopicの表示
+def get_player_topic(
+        db:Session,
+        player_id:int
+):
+    sql = text(
+        """
+        SELECT Topics.topic_text FROM Players
+        INNER JOIN Topics ON Players.assigned_topic_id = Topics.id
+        WHERE Players.id = :player_id
+        """
+    )
+    params = {"player_id": player_id}
+    print(f"SQL: {sql}\nParams: {params}")
+    result = db.execute(sql, params).mappings().first()
+    
+    print(f"DB操作の結果: {result}")
+    
+    return result
+
+
+        # SELECT Topics.topic_text FROM Topics
+        # INNER JOIN (SELECT assigned_topic_id FROM Players WHERE id = :player_id) as selected_player
+        # ON Topics.id = selected_player.assigned_topic_id
--- db/schema.sql
+++ db/schema.sql
@@ -24,7 +24,9 @@ CREATE TABLE Topic_pairs(
 
 CREATE TABLE Games(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    theme_id INT,
+    FOREIGN KEY(theme_id) REFERENCES Themes(id)
 );
 
 CREATE TABLE Players(
@@ -32,7 +34,9 @@ CREATE TABLE Players(
     name VARCHAR(64),
     assigned_topic_id INT,
     keyword VARCHAR(64),
-    FOREIGN KEY (assigned_topic_id) REFERENCES Topics(id)
+    game_id INT,
+    FOREIGN KEY (assigned_topic_id) REFERENCES Topics(id),
+    FOREIGN KEY (game_id) REFERENCES Games(id)
 );
 
 
